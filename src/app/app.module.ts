dc8c48421bd8ffdfe6ab344bc751b33edb3fb625
Tendai Mutunhire <tmutunhire@gmail.com>
2017-02-14 22:22:23 +0200

    add beer component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,12 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { UserModule } from './users/users.module';
 import { UserComponent } from './users/user.component';
+import { BeerComponent } from './beer/beer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BeerComponent
   ],
   imports: [
     BrowserModule,
