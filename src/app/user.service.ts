9a3d6d6aa2d371e7a64aa96024e4ad69286c883b
Tendai Mutunhire <tmutunhire@gmail.com>
2017-02-14 22:53:48 +0200

    add and use a User Service

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -1,6 +1,13 @@
 import {  Injectable } from '@angular/core';
 
-
+@Injectable()
 export class UserService {
 
+  constructor() {
+
+  }
+
+  getHi() {
+    return 'Nihao';
+  }
 }
--- src/app/users/user.component.ts
+++ src/app/users/user.component.ts
@@ -1,9 +1,26 @@
 import { Component } from '@angular/core';
+import { Inject } from '@angular/core';
+
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'user',
-  templateUrl: './user.component.html'
+  templateUrl: './user.component.html',
+  providers: [UserService]
 })
 export class UserComponent {
+  title: string;
+  greeting: any;
   author = "Tendai Mutunhire";
+  userService: UserService;
+
+  constructor(userService: UserService) {
+    this.userService = userService;
+  }
+
+  ngOnInit() {
+    console.log("user component says hello");
+    console.log(this.userService.getHi());
+    this.greeting = this.userService.getHi();
+  }
 }
