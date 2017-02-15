28b352c9bb1806d12d1fd972ee39b599c2247074
Tendai Mutunhire <tmutunhire@gmail.com>
2017-02-15 23:17:54 +0200

    remarkable, just remarkable

--- src/app/animation/animation.component.ts
+++ src/app/animation/animation.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, trigger, state, style } from '@angular/core';
 
 @Component({
   selector: 'app-animation',
+  animations: [
+    trigger('cognify', [
+      state('go', style({
+        'background-color':'green'
+      }))
+    ])
+  ],
   templateUrl: './animation.component.html',
   styleUrls: ['./animation.component.css']
 })
 export class AnimationComponent implements OnInit {
-
+  shade: string;
   constructor() { }
 
   ngOnInit() {
+  }
+
+  changeShade() {
+    console.log("changing the shade");
   }
 
 }
